ae490b116c003a6496658faf3c3f548650b2960a
marcos041220 <ramosmarkinhu@outlook.com>
2020-03-13 22:45:21 -0300

    finish

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase para o cebolinha");
+            String frase = Console.ReadLine();
+            frase = frase.Replace("r","l").Replace("R","L");
+            Console.WriteLine(frase);
+            Console.WriteLine($"Você escreveu uma frase e o cebolinha repetiu como {frase}");
+            Console.ReadLine();
+        }
+    }
+}
